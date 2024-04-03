0976c102addaf92ce210843393facce5c8ebd0d2
Przemek <ikaria@poczta.onet.pl>
2024-04-03 21:13:25 +0200

    Created class and endpoint to sign up

--- backend/src/main/java/com/app/auth/AuthController.java
+++ backend/src/main/java/com/app/auth/AuthController.java
@@ -1,7 +1,11 @@
 package com.app.auth;
 
+import com.app.model.User;
+import com.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/signin")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
         String token = "Test"; //todo generate jwt
         return new ResponseEntity<>(token, HttpStatus.OK);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> registerUser(@RequestBody SignUpDto signUpDto){
+        String email = signUpDto.getEmail();
+        if(userRepository.findByEmail(email).isPresent()){
+            return new ResponseEntity<>("User with this email already exists", HttpStatus.FORBIDDEN);
+        }
+
+        User user = User.builder()
+                .firstname(signUpDto.getFirstName())
+                .lastname(signUpDto.getLastName())
+                .email(signUpDto.getEmail())
+                .password(passwordEncoder.encode(signUpDto.getPassword()))
+                .build();
+
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.CREATED);
     }
 }
--- backend/src/main/java/com/app/config/SecurityConfiguration.java
+++ backend/src/main/java/com/app/config/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +24,10 @@ public class SecurityConfiguration {
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
