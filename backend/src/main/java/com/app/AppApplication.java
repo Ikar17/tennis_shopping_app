d70848ea2b6a3dd868e099e79b6155ef2286dabf
Przemek <ikaria@poczta.onet.pl>
2025-12-03 02:02:55 +0100

    .env setup

--- backend/src/main/java/com/app/AppApplication.java
+++ backend/src/main/java/com/app/AppApplication.java
@@ -2,11 +2,19 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Ustawienie wszystkich załadowanych zmiennych jako właściwości systemowe
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(AppApplication.class, args);
 	}
 
